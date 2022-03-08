8603aa7756ee8557c3d59577474140464b47f902
Pinchon Léo <leopinchonspa@gmail.com>
2022-03-08 11:19:33 +0100

    Post dans article + les tables many to many + vérif avec Joi

--- routes/articles.js
+++ routes/articles.js
@@ -1,16 +1,15 @@
 const express = require('express')
+const app = express()
 const mysql = require('../config/db')
 
 const articlesRouter = express.Router()
 const Articles = require('../models/articles')
+const ArticlesSecteurs = require('../models/articles_secteurs')
+const ArticlesSousCats = require('../models/articles_sousCategories')
+const ArticlesVilles = require('../models/articles_villes')
 
-// router.get("/", (req,res) =>{
-//     res.status(200).send("Dans Articles")
-// })
-
+// READ ALL With FILTERS
 articlesRouter.get('/', (req, res) => {
-  // const sql = 'SELECT art.titre, art.intro, art.para1, art.para2, art.para3, art.avantage, art.lien1, art.lien2, art.lien3, art.image,vil.nom_ville, reg.nom_region, ssc.nom_sous_categorie, cat.nom_categorie, sec.nom_secteur FROM articles as art LEFT JOIN secteurs_has_articles as sec_art ON art.id_article = sec_art.article_id LEFT JOIN secteurs AS sec ON sec_art.secteur_id= sec.id_secteur LEFT JOIN sous_categories AS ssc ON art.sous_categorie_id=ssc.id_sous_categorie LEFT JOIN categories AS cat ON ssc.categorie_id=cat.id_categorie LEFT JOIN villes_has_articles as vil_art ON art.id_article = vil_art.article_id LEFT JOIN villes as vil ON vil_art.ville_id=vil.id_ville LEFT JOIN regions as reg ON vil.region_id = reg.id_region;'
-
   const { search, ville, categorie, sousCategorie } = req.query
 
   Articles.findMany({ filters: { search, ville, categorie, sousCategorie } })
@@ -21,27 +20,17 @@ articlesRouter.get('/', (req, res) => {
       console.error(err)
       res.status(500).send('Error retrieving data from articles')
     })
-
-  // if(err) {
-  //     res.status(500).send("Error retrieving data from articles")
-  //     console.error(err)
-  // } else {
-  //     console.table(result)
-  //     res.status(200).json(result)
-  // }
 })
 
-// Routes POST
-
-// INTEGRATION ARTICLE ROMAIN - a verifier RETRAIT DES 3 PARAGRAPHES car utilisation de TINY
+// Post One
 articlesRouter.post('/', (req, res) => {
+  let idArticle = null
+  let  createdArtSecRes = null
+  let createdArtcilesRes = null
   const {
     titre,
     intro,
-    type,
     para1,
-    para2,
-    para3,
     avantage,
     lien1,
     lien2,
@@ -49,58 +38,70 @@ articlesRouter.post('/', (req, res) => {
     image,
     visible,
     user_id,
-    sous_categorie_id
+    secteur_id,
+    sous_categorie_id,
+    ville_id
   } = req.body
 
-  sql =
-    'INSERT INTO articles (titre, intro, type, para1, para2, para3, avantage, lien1, lien2, lien3, image, visible, user_id, sous_categorie_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?);'
+  const errorArt = Articles.validate({
+    titre,
+    intro,
+    para1,
+    avantage,
+    lien1,
+    lien2,
+    lien3,
+    image,
+    visible,
+    user_id
+  })
 
-  mysql.query(
-    sql,
-    [
+  const errorArtSec = ArticlesSecteurs.validate({ secteur_id })
+  const errorArtVille = ArticlesVilles.validate({ ville_id })
+  const errorArtSousCat = ArticlesSousCats.validate({ sous_categorie_id })
+
+  // if  (errorArt || errorArtSec || errorArtVille || errorArtSousCat){
+  //   res.status(422).json( errorArt && { validationErrors: errorArt.details } || errorArtSec && { validationErrors: errorArtSec.details } || errorArtVille && { validationErrors: errorArtVille.details } || errorArtSousCat && { validationErrors: errorArtSousCat.details })
+  
+  if (errorArt) {
+    res.status(422).json({ validationErrors: errorArt.details })
+  } else if (errorArtSec) {
+    res.status(422).json({ validationErrors: errorArtSec.details })
+  } else if (errorArtVille) {
+    res.status(422).json({ validationErrors: errorArtVille.details })
+  } else if (errorArtSousCat) {
+    res.status(422).json({ validationErrors: errorArtSousCat.details })
+  } else {
+    Articles.create({
       titre,
       intro,
-      type,
       para1,
-      para2,
-      para3,
       avantage,
       lien1,
       lien2,
       lien3,
       image,
       visible,
-      user_id,
-      sous_categorie_id
-    ],
-    (err, result) => {
-      if (err) {
-        console.log(err)
-        res.status(500).send('Error saving articles')
-      } else {
-        console.log(result)
-        const id = result.insertId
-        const createdArticles = {
-          id,
-          titre,
-          intro,
-          type,
-          para1,
-          para2,
-          para3,
-          avantage,
-          lien1,
-          lien2,
-          lien3,
-          image,
-          visible,
-          user_id,
-          sous_categorie_id
-        }
-        res.status(201).json(createdArticles)
-      }
-    }
-  )
+      user_id
+    })
+      .then(({ id, ...createdArticles }) => {
+        console.log(id)
+        idArticle = id
+        createdArtcilesRes=createdArticles
+      })
+      .then(() =>  Promise.all([
+        secteur_id && ArticlesSecteurs.create(secteur_id, idArticle),
+        ArticlesSousCats.create(idArticle, sous_categorie_id),
+        ArticlesVilles.create(ville_id, idArticle)
+      ])
+      .then(([sec, sous, ville]) => {
+        res.status(201).json({createdArtcilesRes,sec,sous,ville})
+      })
+      
+      )
+  }
+
+  
 })
 
 module.exports = articlesRouter
