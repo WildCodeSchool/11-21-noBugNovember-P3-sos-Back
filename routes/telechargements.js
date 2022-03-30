23b5496252c725320c04939488c119acd7be8887
Pinchon Léo <leopinchonspa@gmail.com>
2022-03-30 15:25:51 +0200

    commit avant le clean

--- routes/telechargements.js
+++ routes/telechargements.js
@@ -52,7 +52,6 @@ telechargementsRouter.post('/', (req, res) => {
 })
 
 
-
 // UPDATE ONE
 telechargementsRouter.put('/:id', (req, res) => {
   let existingtelechargement = null
@@ -80,8 +79,6 @@ telechargementsRouter.put('/:id', (req, res) => {
     })
 })
 
-
-
 // DELETE ONE
 telechargementsRouter.delete('/:id', (req, res) => {
   Telechargement.destroy(req.params.id)
@@ -98,71 +95,6 @@ telechargementsRouter.delete('/:id', (req, res) => {
     })
 })
 
-// router.get('/', (req, res) => {
-//     const sql = 'SELECT * FROM telechargements'
-//     mysql.query(sql, (err, result) => {
-//         if (err) {
-//             res.status(500).send('Error retrieving data from telechargements')
-//             console.error(err)
-//         } else {
-//             console.table(result)
-//             res.status(200).json(result)
-//         }
-//     })
-// })
-
-// // Routes POST
-
-// router.post('/', (req, res)=>{
-//     const { prenom_telechargement, nom_telechargement, mail_telechargement, ville_telechargement, article_id } = req.body
-
-//     const sql = 'INSERT INTO telechargements ( prenom_telechargement, nom_telechargement, mail_telechargement, ville_telechargement, article_id ) VALUES (?,?,?,?,?);'
-//     mysql.query(sql, [ prenom_telechargement, nom_telechargement, mail_telechargement, ville_telechargement, article_id ], (err, result)=>{
-//         if (err) {
-//             console.log(err)
-//             res.status(500).send('Error saving telechargements')
-//         } else {
-//             console.log(result)
-//             const id = result.insertId
-//             const createdTelechargements = { id, prenom_telechargement, nom_telechargement, mail_telechargement, ville_telechargement, article_id }
-//             res.status(201).json(createdTelechargements)
-//         }
-//     })
-// })
-
-// // routes PUT
-
-// router.put('/:id', (req,res)=>{
-//     const telechargementId = req.params.id
-//     const telechargementPropsUpdate = req.body
-//     const sql ='UPDATE telechargements SET ? WHERE id_telechargement= ?'
-//     mysql.query ( sql, [ telechargementPropsUpdate,telechargementId ], (err,result)=>{
-//         if (err){
-//             console.log(err)
-//             res.status(500).send('Error update telechargement')
-//         } else {
-//             console.log(result)
-//             const updateTelechargements = { telechargementId, telechargementPropsUpdate}
-//             res.status(200).json(updateTelechargements)
-//         }
-//     })
-// })
-
-// //routes DELETE
-
-// router.delete('/:id', (req,res)=>{
-//     const telechargementId = req.params.id
-//     const sql ='DELETE FROM telechargements WHERE id_telechargement= ?'
-//     mysql.query ( sql, [ telechargementId ], (err,result)=>{
-//         if (err){
-//             console.log(err)
-//             res.status(500).send('Error delete telechargement')
-//         } else {
-//             console.log(result)
-//             res.status(200).json(`the id ${telechargementId} was successfully delete`)
-//         }
-//     })
-// })
 
 
 module.exports = telechargementsRouter
